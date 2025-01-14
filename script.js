8b1e89058ee71acf4378497cbb365ebd84f0d6ff
R.L.D.T.H. Surasinghe <surasinghe_rldth_e23@engug.ruh.ac.lk>
2025-01-14 23:57:32 +0530

    refactor : update script.js

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ const temperature_element = document.getElementById('temperature');
 const humidity_element = document.getElementById('humidity');
 const wind_speed_element = document.getElementById('wind-speed');
 const country_element = document.getElementById('country');
-const town_element = document.getElementById('town');
+const town_element = document.getElementById('city');
 
 
 const apiKey = "fa799a87d813b55dd354265a648ffd54";
 
-var city = "Washington";
+const urlParams = new URLSearchParams(window.location.search);
 
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&city=${city}&units=metric`;
+var city = urlParams.get('city');
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${city}&units=metric`;
 
 fetch(apiURL)
 .then((response) =>{
@@ -25,6 +27,28 @@ fetch(apiURL)
 })
 
 .then((data) =>{
-    console.log(data);
-}
-)
+    console.log("Weather Data:", data);
+
+    const weather = data.weather[0].main;
+    const weather_description = data.weather[0].description;
+    const weather_icon = data.weather[0].icon;
+    const temperature = data.main.temp;
+    const humidity = data.main.humidity;
+    const wind_speed = data.wind.speed;
+    const country = data.sys.country;
+    const town = data.name;
+
+    console.log("Weather:", weather_description);
+
+
+    weather_element.innerText = weather;
+    country_element.innerText = country;
+    town_element.innerText = town;
+    temperature_main_element.innerText = temperature;
+    weather_description_element.innerText = weather_description;
+    humidity_element.innerText = `${humidity}%`;
+    wind_speed_element.innerText = `${wind_speed}m/s`;
+    temperature_element.innerText = `${temperature}°C`;
+
+    weather_icon_element.src = `https://openweathermap.org/img/wn/${weather_icon}@2x.png`;
+});
